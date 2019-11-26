1acc585242be662151a063d5490e591c7b3deeac
jpch89 <jpch89@outlook.com>
2019-11-26 08:36:37 +0800

    add chapter 2

--- mysite/account/admin.py
+++ mysite/account/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import UserProfile
+from .models import UserInfo, UserProfile
 
 
 class UserProfileAdmin(admin.ModelAdmin):
@@ -8,3 +8,11 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_filter = ('phone', )
 
 admin.site.register(UserProfile, UserProfileAdmin)
+
+
+@admin.register(UserInfo)
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ('user', 'school', 'company', 'profession', 'address', 'aboutme', 'photo')
+    list_filter = ('school', 'company', 'profession')
+
+# admin.site.register(UserInfo, UserInfoAdmin)
