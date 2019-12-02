add19b4d5c4078cec7c8a596e8bf444d18a5ebba
jpch89 <jpch89@outlook.com>
2019-12-02 09:38:13 +0800

    add 4.3

--- mysite/article/list_views.py
+++ mysite/article/list_views.py
@@ -1,11 +1,24 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
 from .models import ArticleColumn, ArticlePost
 
 
-def article_titles(request):
-    articles_title = ArticlePost.objects.all()
+def article_titles(request, username=None):
+    if username:
+        user = User.objects.get(username=username)
+        articles_title = ArticlePost.objects.filter(author=user)
+        try:
+            userinfo = user.userinfo
+        except:
+            userinfo = None
+    else:
+        articles_title = ArticlePost.objects.all()
     paginator = Paginator(articles_title, 2)
     page = request.GET.get('page')
     try:
@@ -17,7 +30,13 @@ def article_titles(request):
     except EmptyPage:
         current_page = paginator.page(paginator.num_pages)
         articles = current_page.object_list
-    
+
+    if username:
+        return render(request, 'article/list/author_articles.html', {
+            'articles': articles, 'page': current_page,
+            'userinfo': userinfo, 'user': user
+        })
+
     return render(request, 'article/list/article_titles.html', {
         'articles': articles,
         'page': current_page,
@@ -27,3 +46,22 @@ def article_titles(request):
 def article_detail(request, id, slug):
     article = get_object_or_404(ArticlePost, id=id, slug=slug)
     return render(request, 'article/list/article_content.html', {'article': article})
+
+
+@csrf_exempt
+@require_POST
+@login_required(login_url='/account/login/')
+def like_article(request):
+    article_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if article_id and action:
+        try:
+            article = ArticlePost.objects.get(id=article_id)
+            if action == 'like':
+                article.users_like.add(request.user)
+                return HttpResponse('1')
+            else:
+                article.users_like.remove(request.user)
+                return HttpResponse('2')
+        except:
+            return HttpResponse('0')
