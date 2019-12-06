b72596646cf79d1bf8f45993238f78de09c88a6e
jpch89 <jpch89@outlook.com>
2019-12-06 18:04:50 +0800

    add chapter 4

--- mysite/article/list_views.py
+++ mysite/article/list_views.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.csrf import csrf_exempt
@@ -69,12 +70,17 @@ def article_detail(request, id, slug):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    
+    article_tags_ids = article.article_tag.values_list('id', flat=True)
+    similar_articles = ArticlePost.objects.filter(article_tag__in=article_tags_ids).exclude(id=article.id)
+    similar_articles = similar_articles.annotate(same_tags=Count('article_tag')).order_by('-same_tags', '-created')[:4]
 
     return render(request, 'article/list/article_content.html', {
         'article': article,
         'total_views': total_views,
         'most_viewed': most_viewed,
-        'comment_form': comment_form})
+        'comment_form': comment_form,
+        'similar_articles': similar_articles})
 
 
 @csrf_exempt
