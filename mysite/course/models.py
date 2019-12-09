b757cc2f6fb611b94c2001793d062c6ca3353f06
jpch89 <jpch89@outlook.com>
2019-12-09 17:35:09 +0800

    add 7.1.2

--- mysite/course/models.py
+++ mysite/course/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
+from slugify import slugify
 
-# Create your models here.
+
+class Course(models.Model):
+    # 外键就是一对多，一个用户对应多个课程，定义在多的一侧
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses_user')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created', )
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Course, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.title
--- mysite/course/views.py
+++ mysite/course/views.py
@@ -1,5 +1,19 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.models import User
+from django.views.generic import ListView, TemplateView
+
+from .models import Course
 
 
 class AboutView(TemplateView):
     template_name = 'course/about.html'
+
+
+class CourseListView(ListView):
+    model = Course
+    context_object_name = 'courses'
+    template_name = 'course/course_list.html'
+    # queryset = Course.objects.filter(user=User.objects.get(username='jpch89'))
+
+    def get_queryset(self):
+        qs = super(CourseListView, self).get_queryset()
+        return qs.filter(user=User.objects.get(username='jpch89'))
