8389bd6bf18bb65ca45a10c936511f0554c8bbff
jpch89 <jpch89@outlook.com>
2019-12-09 09:00:55 +0800

    add 5.2.1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'account',
     'article',
     'image',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
@@ -143,3 +144,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 REDIS_DB = 0
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
@@ -26,3 +28,5 @@ urlpatterns = [
     path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
     path('image/', include('image.urls', namespace='image')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
